89d44cc2011fab263c5d83f29a71358e20b1e1cd
Daniel Gomes <daniel.gomes@itsector.pt>
2019-01-22 15:08:36 +0000

    Setup Crashlytics

--- app/src/main/java/pt/dbmg/mobiletaiga/MainActivity.kt
+++ app/src/main/java/pt/dbmg/mobiletaiga/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.navigation.fragment.NavHostFragment
 import com.apollographql.apollo.ApolloClient
+import com.crashlytics.android.Crashlytics
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+import io.fabric.sdk.android.Fabric
 import kotlinx.android.synthetic.main.activity_main.drawer_layout
 import kotlinx.android.synthetic.main.activity_main.nav_view
 import kotlinx.android.synthetic.main.app_bar_main2.nav_button_menu
@@ -35,6 +37,7 @@ class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionList
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(activity_main)
         setSupportActionBar(toolbar)
         nav_button_menu.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
